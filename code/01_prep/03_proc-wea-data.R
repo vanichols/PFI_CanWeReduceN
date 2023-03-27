f7e9aa003faf75314523a451cd335ec1fdb31605
vanichols <virginia.nichols@gmail.com>
2023-03-27 08:24:31 -0600

    summarised and wrote weather

--- code/01_prep/03_proc-wea-data.R
+++ code/01_prep/03_proc-wea-data.R
@@ -7,58 +7,104 @@ my_wea_files <- fs::dir_ls("data_wea/raw/", regexp = "\\.csv$")
 w <- 
   my_wea_files %>% 
   map_dfr(read_csv) |> 
-  janitor::clean_names() 
+  janitor::clean_names() |> 
+  mutate(t2m = weathermetrics::celsius.to.fahrenheit(t2m),
+         prectotcorr = prectotcorr * 0.0393701) #--change to F and inches
 
-# long term ---------------------------------------------------------------
+# temperature ---------------------------------------------------------------
 
 t_lt <- 
   w |> 
-  group_by(lon, lat, city, doy) |> 
-  summarise(t2m = mean(t2m)) |> 
-  mutate(time_frame = "1982-2022")
+  group_by(lon, lat, city, doy) |> #--can't group by mm and dd, not always the same doy 
+  summarise(t_lt = mean(t2m)) 
 
 t_22 <- 
   w |> 
   filter(year == 2022) |> 
-  group_by(lon, lat, city, doy) |> 
-  summarise(t2m = mean(t2m)) |> 
-  mutate(time_frame = "2022")
+  group_by(lon, lat, city, mm, dd, doy) |> 
+  summarise(t_22 = mean(t2m)) 
 
-t <- 
+t_final <- 
   t_lt |> 
-  bind_rows(t_22)
+  left_join(t_22) |>
+  pivot_longer(c(t_lt, t_22))
 
-t |> 
-  filter(city == "hastings") |> 
-  ggplot(aes(doy, t2m)) + 
-  geom_line(aes(color = time_frame))
+# precip ------------------------------------------------------------------
 
-t |> 
-  mutate(t2m = weathermetrics::celsius.to.fahrenheit(t2m),
-         t2m = ifelse(t2m <0, 0, t2m)) |> 
-  pivot_wider(names_from = time_frame,
-              values_from = t2m) |> 
-  janitor::clean_names() |>
+p <- 
+  w |> 
+  group_by(city, year) |> 
+  mutate(
+    precip_in = prectotcorr,
+    cumprecip_in = cumsum(precip_in)) 
+
+#--long term
+p_lt <- 
+  p |>  
+  group_by(lon, lat, city, doy) |> 
+  summarise(cp_lt = mean(cumprecip_in)) 
+
+p_22 <- 
+  p |> 
+  filter(year == 2022) |> 
+  group_by(lon, lat, city, mm, dd, doy) |> 
+  summarise(cp_22 = mean(cumprecip_in)) 
+
+p_final <- 
+  p_lt |> 
+  left_join(p_22) |> 
+  pivot_longer(c(cp_lt, cp_22))
+
+
+# temperature, cum --------------------------------------------------------
+
+ct_lt <- 
+  t_lt |> 
   group_by(city) |> 
-  mutate(cum_lt = cumsum(x1982_2022),
-         cum_22 = cumsum(x2022)) |> 
-  filter(city == "albion") |> 
-  ggplot() + 
-  geom_line(aes(doy, cum_22), color= "red") + 
-  geom_line(aes(doy, cum_lt), color= "black") +
-  facet_wrap(~city)
+  mutate(
+    t_lt = ifelse(t_lt < 0, 0, t_lt),
+    ct_lt = cumsum(t_lt)) |> 
+  select(-t_lt)
 
-t |> 
-  mutate(t2m = weathermetrics::celsius.to.fahrenheit(t2m),
-         t2m = ifelse(t2m <0, 0, t2m)) |> 
-  pivot_wider(names_from = time_frame,
-              values_from = t2m) |> 
-  janitor::clean_names() |>
+ct_22 <- 
+  t_22 |> 
   group_by(city) |> 
-  mutate(cum_lt = x1982_2022,
-         cum_22 = x2022) |> 
+  mutate(
+    t_22 = ifelse(t_22 < 0, 0, t_22),
+    ct_22 = cumsum(t_22)) |> 
+  select(-t_22)
+
+ct_final <- 
+  ct_lt |> 
+  left_join(ct_22) |> 
+  pivot_longer(c(ct_lt, ct_22))
+
+
+# figurability ------------------------------------------------------------
+
+p_final |> 
   filter(city == "albion") |> 
-  ggplot() + 
-  geom_line(aes(doy, cum_22), color= "red") + 
-  geom_line(aes(doy, cum_lt), color= "black") +
-  facet_wrap(~city)
+  ggplot(aes(doy, value)) +
+  geom_line(aes(color = name), size = 3)
+
+t_final |> 
+  filter(city == "albion") |> 
+  ggplot(aes(doy, value)) +
+  geom_line(aes(color = name), size = 3)
+
+ct_final |> 
+  filter(city == "albion") |> 
+  ggplot(aes(doy, value)) +
+  geom_line(aes(color = name), size = 3)
+
+
+# write -------------------------------------------------------------------
+
+t_final |> 
+  write_csv("data_wea/temperature-F.csv")
+
+ct_final |> 
+  write_csv("data_wea/cum-temperature-F.csv")
+
+p_final |> 
+  write_csv("data_wea/cum-precip-in.csv")
